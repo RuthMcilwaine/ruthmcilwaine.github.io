ccbbb44ad5a8f58d53fefe01f80d7fb7e5144902
Ruth.Mcilwaine <ruth.mcilwaine@myob.com>
2025-05-27 21:52:49 +1000

    fix responsiveness

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function loadComponent(url, elementId) {
+function loadComponent(url, elementId, callback) {
     fetch(url)
         .then((response) => {
             if (!response.ok) {
@@ -8,6 +8,9 @@ function loadComponent(url, elementId) {
         })
         .then((data) => {
             document.getElementById(elementId).innerHTML = data;
+            if (typeof callback === "function") {
+                callback();
+            }
         })
         .catch((error) => {
             console.error(`Error loading ${elementId}:`, error);
@@ -15,6 +18,19 @@ function loadComponent(url, elementId) {
 }
 
 window.onload = function () {
-    loadComponent('../components/header.html', 'header');
+    console.log("Loaded");
+    loadComponent('../components/header.html', 'header', function () {
+        const hamburger = document.querySelector(".hamburger");
+        const menu = document.querySelector(".menu");
+
+        if (hamburger && menu) {
+        hamburger.addEventListener("click", () => {
+            menu.classList.toggle("show");
+        });
+        } else {
+            console.error('Hamburger or menu not found in DOM')
+        }
+    });
+    console.log("Loaded after");
     loadComponent('../components/footer.html', 'footer');
 };
